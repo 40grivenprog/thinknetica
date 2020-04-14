e9412ed235663b151a41d47021c111d1ec9d7883
40grivenprog <43151399+40grivenprog@users.noreply.github.com>
2020-04-14 23:30:16 +0300

    Fix quadric.rb

--- lesson1/quadric.rb
+++ lesson1/quadric.rb
@@ -9,16 +9,14 @@ c = gets.chomp.to_i
 desc = b**2 - 4 * a * c
 
 def find_solution(a, b, _c, desc)
-  x1 = (-b + Math.sqrt(desc)) / (2 * a)
-  x2 = (-b - Math.sqrt(desc)) / (2 * a)
-  puts "Первый корень: #{x1}"
-  puts "Второй корень: #{x2}"
+	if desc.zero?
+  "Один корень:#{(-b) / (2 * a)}"
+	elsif desc.negative?
+  	'Нет решения'
+	else
+  	x1 = (-b + Math.sqrt(desc)) / (2 * a)
+  	x2 = (-b - Math.sqrt(desc)) / (2 * a)
+  	"Первый корень: #{x1}, второй корень: #{x2}"
+	end
 end
-
-if desc == 0
-  puts "Один корень:#{(-b) / (2 * a)}"
-elsif desc.negative?
-  puts 'Нет решения'
-else
-  find_solution(a, b, c, desc)
-end
+puts [a,b,c].select(&:zero?).empty?  ? find_solution(a, b, c, desc) : "Не является квадратным уравнением"
