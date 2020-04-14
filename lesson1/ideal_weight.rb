26e4853bbb807f0e6009d909f0a4487c0b402df1
40grivenprog <43151399+40grivenprog@users.noreply.github.com>
2020-04-14 23:10:34 +0300

    fix ideal_weight.rb

--- lesson1/ideal_weight.rb
+++ lesson1/ideal_weight.rb
@@ -4,6 +4,7 @@ puts 'Введите ваше имя:'
 name = gets.chomp
 puts 'Введите свой рост:'
 length = gets.chomp.to_i
+return if length.negative?
 ideal_weight = (length - 110) * 1.15
 if ideal_weight.negative?
   puts 'Ваш вес уже оптимальный'
